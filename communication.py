c4d23710a6359f9f1ba39caad2afc75b758b2efb
cristianrossettliotto <cristian.rossett@hotmail.com>
2024-09-27 20:27:38 -0300

    temp

--- communication.py
+++ communication.py
@@ -1,5 +1,6 @@
 import sockets
 import json
+from time import sleep
 from datetime import datetime, timedelta
 
 def receive_packtes(communication_socket, stop_event, local_ip, messages_to_validate):
@@ -7,12 +8,16 @@ def receive_packtes(communication_socket, stop_event, local_ip, messages_to_vali
         try:
             data, addr = communication_socket.recvfrom(1024)
             message =  json.loads(data.decode('utf-8'))
+            current_time = datetime.now()
+            expiration_time = current_time + timedelta(seconds=15)
+            message['expiration_time'] = expiration_time
 
             if addr[0] != local_ip and not addr[0].startswith('127.'):
-                current_time = datetime.now()
-                expiration_time = current_time + timedelta(seconds=15)
-                message['expiration_time'] = expiration_time
                 messages_to_validate.append(message)
+            else:
+                sleep(1)
+                messages_to_validate.append(message)
+
         except BlockingIOError:
             continue
         except ValueError:
@@ -29,5 +34,5 @@ def send_packets(communication_socket, stop_event, local_ip):
             break
 
         communication_socket.sendto(
-            json.dumps({'content': userInput, 'origin': local_ip, 'author': '','validation_count': 0, 'expiration_time': ''}).encode('utf-8'), 
+            json.dumps({'content': userInput, 'origin': local_ip, 'author': '','validation_count': 0, 'expiration_time': ''}, indent=4, sort_keys=True, default=str).encode('utf-8'), 
             (sockets.broadcast_address, sockets.communication_port))
--- validation.py
+++ validation.py
@@ -4,12 +4,12 @@ from time import sleep
 
 def request_message_validation(list_of_addresses, messages_to_validate):
     sleep(1)
-    for message_ip in messages_to_validate:
-        message_received = message_ip[0]
-        ip_from_sender = message_ip[1]
+    for message in messages_to_validate:
+        message_to_send = message
+        ip_from_sender = message['origin']
         for address in list_of_addresses:
-            if ip_from_sender != address:
-                sockets.validation_socket.sendto(json.dumps(message_received).encode('utf-8'), (address, sockets.validtion_port))
+            # if ip_from_sender != address:
+            sockets.validation_socket.sendto(json.dumps(message_to_send, indent=4, sort_keys=True, default=str).encode('utf-8'), (address, sockets.validtion_port))
 
 
 def validate_other_node_messages(stop_event, validated_messages, messages_to_validate):
@@ -17,14 +17,17 @@ def validate_other_node_messages(stop_event, validated_messages, messages_to_val
     while not stop_event.is_set():
         try:
             data, addr = sockets.validation_socket.recvfrom(1024)
-            message_to_validate = json.loads(data.decode('utf-8'))
+            message_received_to_validate = json.loads(data.decode('utf-8'))
+
+            print(f'Type: {type(message_received_to_validate)}')
+            print(f'Message: {message_received_to_validate}')
 
             for message in messages_to_validate:
-                if message['content'] == message_to_validate['content']:
+                if message['content'] == message_received_to_validate['content']:
                     flag = True
 
             for message in validated_messages:
-                if message['content'] == message_to_validate['content']:
+                if message['content'] == message_received_to_validate['content']:
                     flag = True
 
             if flag:
