86b9cfd1c329439bf22333d8fd06efda9e8e2bb5
cristianrossettliotto <cristian.rossett@hotmail.com>
2024-09-27 20:45:11 -0300

    temp

--- daemons.py
+++ daemons.py
@@ -6,7 +6,7 @@ def verify_messages_to_validate_queue(stop_event, list_of_addresses, messages_to
     old_messages_to_validate = []
     while not stop_event.is_set():
         if old_messages_to_validate != messages_to_validate:
-            print(f'Method verify_messages_to_validate_queue is calling the method request_message_validation with queue {messamessages_to_validatege}')
+            print(f'Method verify_messages_to_validate_queue is calling the method request_message_validation with queue {messages_to_validate}')
             request_message_validation(list_of_addresses, messages_to_validate)
             old_messages_to_validate = messages_to_validate
         else:
