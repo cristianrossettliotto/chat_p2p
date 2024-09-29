43f6a310569b6a79724ac882ec4d5844993ac5f0
Cristian Rossett Liotto <cristian.rossett@hotmail.com>
2024-09-29 11:00:39 -0300

    temp

--- daemons.py
+++
@@ -1,8 +0,0 @@
-from validation import request_message_validation
-from time import sleep
-
-
-def verify_messages_to_validate_queue(list_of_addresses, messages_to_validate):
-    print(f'DAEMON: UPDATING LOCAL QUEUE WITH {messages_to_validate}')
-    request_message_validation(list_of_addresses, messages_to_validate)
-    old_messages_to_validate = messages_to_validate
