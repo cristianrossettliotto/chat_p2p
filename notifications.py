4fe55fc695d838ae86235d1d0fd8a6051311b408
cristianrossettliotto <cristian.rossett@hotmail.com>
2024-10-02 20:18:37 -0300

    temp

--- notifications.py
+++ notifications.py
@@ -14,18 +14,19 @@ def listen_notifications(stop_event, list_of_addresses, local_ip):
             data, addr = sockets.notification_socket.recvfrom(1024)
             content =  json.loads(data.decode('utf-8'))
 
-            print(f'Content {content}     {type(content)}')
-
-            if addr[0] != local_ip and addr[0] not in list_of_addresses:
-                print(f'Type: {type(content)}    {content}')
-                if type(content) == str:
-                    print('type of string')
-                elif type(content) ==   list:
-                    print('list')
-
-                
+            if addr[0] != local_ip:
                 list_of_addresses.append(addr[0])
-                sockets.notification_socket.sendto(str(list_of_addresses).encode('utf-8'), (sockets.broadcast_address, sockets.notification_port))
+
+            if type(content) == str and data not in list_of_addresses:
+                list_of_addresses.append(addr[0])
+                
+            if type(content) == list:
+                for address in content:
+                    if address not in list_of_addresses:
+                        list_of_addresses.append(address)
+
+            print(f'Final Result: {list_of_addresses}')
+            sockets.notification_socket.sendto(str(list_of_addresses).encode('utf-8'), (sockets.broadcast_address, sockets.notification_port))
         except BlockingIOError:
             continue
         except ValueError:
