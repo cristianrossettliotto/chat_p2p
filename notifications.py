54b053d37832d2ec9e2b894e5e8f116e608fa844
cristianrossettliotto <cristian.rossett@hotmail.com>
2024-10-02 20:22:36 -0300

    temp

--- notifications.py
+++ notifications.py
@@ -12,17 +12,17 @@ def listen_notifications(stop_event, list_of_addresses, local_ip):
     while not stop_event.is_set():
         try:
             data, addr = sockets.notification_socket.recvfrom(1024)
-            content =  json.loads(data.decode('utf-8'))
+            content = json.loads(data.decode('utf-8'))
 
             if addr[0] != local_ip:
                 list_of_addresses.append(addr[0])
 
-            if type(content) == str and content not in list_of_addresses:
+            if type(content) == str and content not in list_of_addresses and content != local_ip:
                 list_of_addresses.append(content)
                 
             if type(content) == list:
                 for address in content:
-                    if address not in list_of_addresses:
+                    if address not in list_of_addresses and address != local_ip:
                         list_of_addresses.append(address)
 
             print(f'Final Result: {list_of_addresses}')
