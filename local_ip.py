5f5d6a5fdd479a95b9abdee996d668dd7956c005
cristianrossettliotto <cristian.rossett@hotmail.com>
2024-10-03 20:35:09 -0300

    temp

--- local_ip.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def get_local_ip():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        try:
-            s.connect(('8.8.8.8', 80))
-            return s.getsockname()[0]
-        except Exception:
-            return '127.0.0.1'
